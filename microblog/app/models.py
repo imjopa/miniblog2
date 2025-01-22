373bcdc4c300a19ce6b9b3f0a8e708e5aa1dc2d8
Jopa <joaopaulocosta.r@hotmail.com>
2025-01-22 12:14:47 -0300

    Avatars

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -28,7 +28,7 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
     
-    def avatar (self, size):
+    def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
