d51da51bf487a798b285ccc8ae3468558bca110e
Jopa <joaopaulocosta.r@hotmail.com>
2025-01-22 11:52:55 -0300

    User page config

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone
 from typing import Optional
+from hashlib import md5
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from flask_login import UserMixin
@@ -26,6 +27,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar (self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 
 @login.user_loader
