762096c3eb1513ffe1f71d2145245496951d2144
Jopa <joaopaulocosta.r@hotmail.com>
2025-01-23 10:32:47 -0300

    User improvements

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -31,6 +31,10 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+    
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
 
 
 @login.user_loader
