aa6f0d6980c06c046efe0a7f15d13870e87cb75e
Jopa <joaopaulocosta.r@hotmail.com>
2025-03-18 13:24:23 -0300

    Dates & Time

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -7,11 +7,13 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from config import Config
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
