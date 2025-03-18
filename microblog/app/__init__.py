b96fe485a4e175c1e11622dc8b0675d69322afbc
Jopa <joaopaulocosta.r@hotmail.com>
2025-03-18 14:18:25 -0300

    Translation to portuguese

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,19 +1,25 @@
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from config import Config
 from flask_moment import Moment
+from flask_babel import Babel
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 moment = Moment(app)
+babel = Babel(app, locale_selector=get_locale)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
